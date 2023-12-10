105f74ad0482e787f75dc4fc5ccafd1e1d5500b0
sphinxscepter <sphinxscepter@163.com>
2023-12-10 17:46:14 +0800

    init

---
+++ src/main/java/com/cy/filesync4j/Filesync4jApplication.java
@@ -0,0 +1,13 @@
+package com.cy.filesync4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Filesync4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Filesync4jApplication.class, args);
+	}
+
+}
